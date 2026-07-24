cce81685a9c632ff3b75754ef87d08526e9ddb54
AbroxAI <savotradingbot@gmail.com>
2026-07-24 14:39:37 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('push', event => {
+  if (!event.data) return;
+  const data = event.data.json();
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: './assets/icons/icon-192x192.png',
+    badge: './assets/icons/icon-72x72.png',
+    data: { url: data.url || './' }
+  });
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow(event.notification.data.url || './'));
+});
